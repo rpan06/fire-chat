397ad7e933fd8e6710294744ba44f1e17219eda4
Rachel Pan <rpan06@gmail.com>
2018-10-11 16:11:48 -0700

    project-setup

---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div className="center">
+        <h1 className="center">🔥 Fire Chat 🔥</h1>
+        <h5>STAY SPICY</h5>
+    </div>
+)
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combinReducers, combineReducers} from 'redux';
+import chatReducer from './chat_reducer'
+
+const rootReducer = combineReducers({
+    chat: chatReducer
+})
+
+export default rootReducer;
